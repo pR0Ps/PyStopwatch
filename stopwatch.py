5062633eddc1a0bf41e13aad7f01a8aa7bb749bf
Carey Metcalfe <carey@cmetcalfe.ca>
2016-07-28 02:33:33 -0400

    Miscellaneous fixes
     - Removed trailing spaces (how did they get there?)
     - Refactored away global variables and "*" import
     - Fixed an incorrect function call
     - Made PEP8-compliant
     - Fixed shebang line to use env

--- stopwatch.py
+++ stopwatch.py
@@ -1,85 +1,86 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-from Tkinter import *
+"""Show a basic stopwatch GUI"""
+
+from Tkinter import Button, Frame, Label, Tk, StringVar
+from Tkinter import LEFT, NO, TOP, X
 import time
 
-root = None
-sw = None
 
-class StopWatch(Frame):  
-    """ Implements a stop watch frame widget. """                                                                
-    def __init__(self, parent=None, **kw):        
-        Frame.__init__(self, parent, kw)
-        self._start = 0.0        
+class StopWatch(Frame):
+    """A frame that tracks and displays elapsed time"""
+
+    def __init__(self, parent=None, **kwargs):
+        Frame.__init__(self, parent, kwargs)
+        self._start = 0.0
         self._elapsedtime = 0.0
         self._running = False
-        self.timestr = StringVar()               
-        self.makeWidgets()      
+
+        # Time display
+        self.timestr = StringVar()
+        Label(self, textvariable=self.timestr).pack(fill=X, expand=NO, pady=2,
+                                                    padx=2)
+        self._set_time(self._elapsedtime)
+
+        # TK-related
+        self._timer = None
         if parent != None:
             parent.title("Stopwatch")
 
-    def makeWidgets(self):                         
-        """ Make the time label. """
-        l = Label(self, textvariable=self.timestr)
-        self._setTime(self._elapsedtime)
-        l.pack(fill=X, expand=NO, pady=2, padx=2)                      
-    
-    def _update(self): 
-        """ Update the label with elapsed time. """
+    def _update(self):
+        """Update the label with elapsed time"""
         self._elapsedtime = time.time() - self._start
-        self._setTime(self._elapsedtime)
+        self._set_time(self._elapsedtime)
         self._timer = self.after(50, self._update)
-    
-    def _setTime(self, elap):
-        """ Set the time string to Minutes:Seconds:Hundreths """
+
+    def _set_time(self, elap):
+        """Set the time string to Minutes:Seconds:Hundreths"""
         minutes = int(elap/60)
         seconds = int(elap - minutes*60.0)
-        hseconds = int((elap - minutes*60.0 - seconds)*100)                
+        hseconds = int((elap - minutes*60.0 - seconds)*100)
         self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
-        
-    def start_stop(self):                                                     
-        """ Start/Stop the stopwatch. """
+
+    def start_stop(self):
+        """Start/Stop the stopwatch"""
         if self._running:
-            self.after_cancel(self._timer)            
-            self._elapsedtime = time.time() - self._start    
-            self._setTime(self._elapsedtime)
+            self.after_cancel(self._timer)
+            self._elapsedtime = time.time() - self._start
+            self._set_time(self._elapsedtime)
             self._running = False
-        else:            
+        else:
             self._start = time.time() - self._elapsedtime
             self._update()
-            self._running = True        
-    
-    def reset(self):                                  
-        """ Reset the stopwatch. """
-        self._start = time.time()         
+            self._running = True
+
+    def reset(self):
+        """Reset the stopwatch"""
+        self._start = time.time()
         self._elapsedtime = 0.0
         if self._running:
-            self.StartStop()
-        self._setTime(self._elapsedtime)
+            self.start_stop()
+        self._set_time(self._elapsedtime)
 
-# Callbacks for keystrokes
-def callbackSS(event):
-    sw.start_stop()
-def callbackRESET(event):
-    sw.reset()
-def callbackQUIT(event):
-    root.quit()
 
-# Main program
 def main():
-    global sw, root
-
+    """Set up the stopwatch and the buttons/keyboard shortcuts to control it"""
     root = Tk()
-    sw = StopWatch(root)
-    sw.pack(side=TOP)
-    
-    Button(root, text='Start/Stop', command=sw.start_stop, takefocus=False).pack(side=LEFT)
-    Button(root, text='Reset', command=sw.reset, takefocus=False).pack(side=LEFT)
-    Button(root, text='Quit', command=root.quit, takefocus=False).pack(side=LEFT)
 
-    root.bind("<space>", callbackSS)
-    root.bind("<BackSpace>", callbackRESET)
-    root.bind("<Escape>", callbackQUIT)
+    # Add stopwatch frame
+    stopwatch = StopWatch(root)
+    stopwatch.pack(side=TOP)
+
+    # Add buttons
+    Button(root, text='Start/Stop',
+           command=stopwatch.start_stop, takefocus=False).pack(side=LEFT)
+    Button(root, text='Reset',
+           command=stopwatch.reset, takefocus=False).pack(side=LEFT)
+    Button(root, text='Quit',
+           command=root.quit, takefocus=False).pack(side=LEFT)
+
+    # Add keyboard shurtcuts
+    root.bind("<space>", lambda x: stopwatch.start_stop())
+    root.bind("<BackSpace>", lambda x: stopwatch.reset())
+    root.bind("<Escape>", lambda x: root.quit())
 
     root.focus_set()
     root.mainloop()
